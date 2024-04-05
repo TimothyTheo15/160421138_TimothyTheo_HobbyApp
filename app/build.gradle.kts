747a0675ef39eb020ffef079d5a40e0101749826
TimothyTheo <160450255+TimothyTheo15@users.noreply.github.com>
2024-04-05 18:20:07 +0700

    Desain Halaman

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.a160421138_timothytheo_hobbyapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.a160421138_timothytheo_hobbyapp"
@@ -43,4 +43,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("com.google.android.material:material:1.11.0-alpha03")
 }
---
+++ app/src/main/java/com/example/a160421138_timothytheo_hobbyapp/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a160421138_timothytheo_hobbyapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a160421138_timothytheo_hobbyapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
